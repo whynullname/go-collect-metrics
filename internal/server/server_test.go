8431d137aac6be6b4b413e4b250fb4d8e940e89a
whynullname <zhenyaxxo@gmail.com>
2025-02-25 16:00:09 +0500

    B! - fix tests bugs

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -12,49 +12,49 @@ import (
 func TestUpdateData(t *testing.T) {
 	tests := []struct {
 		name        string
-		targetUrl   string
+		targetURL   string
 		contentType string
 		methodType  string
 		wantCode    int
 	}{
 		{
 			name:        "positive test #1 - counter metrics",
-			targetUrl:   "/update/counter/someMetrics/500",
+			targetURL:   "/update/counter/someMetrics/500",
 			contentType: "text/plain",
 			methodType:  http.MethodPost,
 			wantCode:    http.StatusOK,
 		},
 		{
 			name:        "positive test #2 - gauge metrics",
-			targetUrl:   "/update/gauge/someMetrics/500",
+			targetURL:   "/update/gauge/someMetrics/500",
 			contentType: "text/plain",
 			methodType:  http.MethodPost,
 			wantCode:    http.StatusOK,
 		},
 		{
 			name:        "test json content type",
-			targetUrl:   "/update/gauge/someMetrics/500",
+			targetURL:   "/update/gauge/someMetrics/500",
 			contentType: "application/json",
 			methodType:  http.MethodPost,
 			wantCode:    http.StatusMethodNotAllowed,
 		},
 		{
 			name:        "test get method",
-			targetUrl:   "/update/gauge/someMetrics/500",
+			targetURL:   "/update/gauge/someMetrics/500",
 			contentType: "text/plain",
 			methodType:  http.MethodGet,
 			wantCode:    http.StatusMethodNotAllowed,
 		},
 		{
 			name:        "test bad url #1",
-			targetUrl:   "/update/",
+			targetURL:   "/update/",
 			contentType: "text/plain",
 			methodType:  http.MethodPost,
 			wantCode:    http.StatusNotFound,
 		},
 		{
 			name:        "test bad url #2",
-			targetUrl:   "/update/someData/someMetrics/500",
+			targetURL:   "/update/someData/someMetrics/500",
 			contentType: "text/plain",
 			methodType:  http.MethodPost,
 			wantCode:    http.StatusBadRequest,
@@ -66,13 +66,14 @@ func TestUpdateData(t *testing.T) {
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			request := httptest.NewRequest(test.methodType, test.targetUrl, nil)
+			request := httptest.NewRequest(test.methodType, test.targetURL, nil)
 			request.Header.Add("Content-Type", test.contentType)
 			w := httptest.NewRecorder()
 
 			serv.UpdateData(w, request)
 
 			res := w.Result()
+			defer res.Body.Close()
 
 			assert.Equal(t, test.wantCode, res.StatusCode)
 		})
