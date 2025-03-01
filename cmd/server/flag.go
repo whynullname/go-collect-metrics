da558ff05f4731f899393184b904adf3973639a9
whynullname <zhenyaxxo@gmail.com>
2025-03-01 09:11:52 +0500

    F! - complete iter5

--- cmd/server/flag.go
+++ cmd/server/flag.go
@@ -1,10 +1,17 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"os"
+)
 
 var endPointAdress string
 
 func parseFlags() {
 	flag.StringVar(&endPointAdress, "a", "localhost:8080", "address and port to run server")
 	flag.Parse()
+
+	if envRunAddr := os.Getenv("ADDRESS"); envRunAddr != "" {
+		endPointAdress = envRunAddr
+	}
 }
